18322ba1ce0a337b9121aff18108c3de2a00c6f2
Mikaël Sévigny <sevignator@hotmail.com>
2024-08-19 23:26:48 -0400

    Add solution for 02-02

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # Enable the window's close button to work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
